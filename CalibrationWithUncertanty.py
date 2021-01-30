25bfbd2e413ad5b2d88d83d184fa45a4871281ec
Lars <larsgudjons@gmail.com>
2021-01-30 17:39:50 +0100

    Calibration als seperate Methode Berechnung der Größe des Markers in Pixeln Zeichen von Mittelpunkten auf Marker

--- CalibrationWithUncertanty.py
+++ CalibrationWithUncertanty.py
@@ -1,216 +1,177 @@
 import numpy as np
 import cv2
+import cv2.aruco as aruco
 import os
 import utils
-
-import pickle
-import glob
-
-cap = cv2.VideoCapture(0)
-
-cap.set(2,1920)
-cap.set(3,1080)
-
-rows = 6
-columns = 9
-saveImages = False
+import math
+import ContourUtils
 
 # termination criteria for Subpixel Optimization
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-squareSize = 30#mm      Beinflusst aber in keiner weise die Matrix
-objp = np.zeros((rows*columns,3), np.float32)
-objp[:,:2] = np.mgrid[0:columns,0:rows].T.reshape(-1,2)*squareSize
-
-# Arrays to store object points and image points from all the images.
 
 
-directory1 = "C:\\Users\\Lars\\Desktop\\TestBilder\\Vorher"
-directory2 = "C:\\Users\\Lars\\Desktop\\TestBilder\\Nachher"
+def calibrateCamera(cap,rows,columns,squareSize,objp,runs,saveImages = False):
+    directory1 = "C:\\Users\\Lars\\Desktop\\TestBilder\\Vorher"
+    directory2 = "C:\\Users\\Lars\\Desktop\\TestBilder\\Nachher"
 
 
-print(os.getcwd())
-print('Path Exists ?')
+    print(os.getcwd())
+    print('Path Exists ?')
 
-print(os.path.exists(directory1))
-print(os.path.exists(directory2))
+    print(os.path.exists(directory1))
+    print(os.path.exists(directory2))
+    if not os.path.exists(directory1) or not os.path.exists(directory2):
+        saveImages = False
 
-allMTX = []
-allDist = []
-allRepErr = []
-runs = 5
+    allMTX = []
+    allDist = []
+    allRepErr = []
 
-for i in range(runs):
-    print('Run ', str(i+1), ' of 5')
-    objpoints = []  # 3d point in real world space
-    imgpoints = []  # 2d points in image plane.
 
-    counter = 0
-    images = []
+    for i in range(runs):
+        print('Run ', str(i+1), ' of 5')
+        objpoints = []  # 3d point in real world space
+        imgpoints = []  # 2d points in image plane.
 
-    #reads in Calib Images
-    while True:
-        succsess, img = cap.read()
-        cv2.imshow("Image", img)
+        counter = 0
+        images = []
 
-        if cv2.waitKey(1) & 0xff == ord('x'):
-            cv2.putText(img, "Captured", (5, 50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0))
+        #reads in Calib Images
+        while True:
+            succsess, img = cap.read()
             cv2.imshow("Image", img)
-            cv2.waitKey(500)
-            images.append(img)                                  #In Array ablegen
-            if saveImages:
-                utils.saveImagesToDirectory(counter,img,directory1)
-            counter += 1
-            print("Captured")
-        if cv2.waitKey(1) & 0xff == ord('q'):
-            break
-    cv2.destroyWindow("Image")
 
-    #shows Images
-    for frame in images:            #Show Images
-        cv2.imshow("Test",frame)
-        cv2.waitKey(100)
-    cv2.destroyWindow("Test")
+            if cv2.waitKey(1) & 0xff == ord('x'):
+                cv2.putText(img, "Captured", (5, 50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0))
+                cv2.imshow("Image", img)
+                cv2.waitKey(500)
+                images.append(img)                                  #In Array ablegen
+                if saveImages:
+                    utils.saveImagesToDirectory(counter,img,directory1)
+                counter += 1
+                print("Captured")
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                break
+        cv2.destroyWindow("Image")
 
-    #findCorners
-    counter2 = 0
-    for img in images:
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        #shows Images
+        for frame in images:            #Show Images
+            cv2.imshow("Test",frame)
+            cv2.waitKey(100)
+        cv2.destroyWindow("Test")
 
-        # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (columns,rows),None)
-        print("FindCorners")
-        # If found, add object points, image points (after refining them)
-        if ret == True:
-            print("        Corners Found")
-            objpoints.append(objp)
-            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-            imgpoints.append(corners2)
+        #findCorners
+        counter2 = 0
+        for img in images:
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-            # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (columns,rows), corners2,ret)
-            if saveImages:
-                utils.saveImagesToDirectory(counter2,img,directory2)
-            counter2 += 1
-            cv2.imshow('img',img)
-            cv2.waitKey(200)
-        else:
-            print("         No Corners Found")
-    print('Found Corners in ' +str(counter2) + ' of ' + str(len(images))+ ' images')
-    print('Detect at least 10 for optimal results')
-    cv2.destroyWindow("img")
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-    print('Matrix:')
-    print(mtx)
-    print('Dist:')
-    print(dist)
-    mean_error = 0
-    meanErrorZeroDist = 0
-    distZero = np.array([0,0,0,0,0],dtype=float)
-    for i in range(len(objpoints)):
-        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, distZero)
-        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
-        meanErrorZeroDist += error
-        if i == 1:
-            pass
-            # print('Soll')
-            # print(imgpoints[i])
-            # print('Nach Reproduktion')
-            # print(imgpoints2)
-    print("Mean error between Ideal Chessboard Corners and Image Corners: {}".format(meanErrorZeroDist / len(objpoints)))
-    for i in range(len(objpoints)):
-        imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-        error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
-        mean_error += error
-        if i == 1:
-            pass
-            # print('Soll')
-            # print(imgpoints[i])
-            # print('Nach Reproduktion')
-            # print(imgpoints2)
-    print("Mean error between projected Objecktpoints using distortion parameters to Points in real Image: {}".format(mean_error / len(objpoints)))
+            # Find the chess board corners
+            ret, corners = cv2.findChessboardCorners(gray, (columns,rows),None)
+            print("FindCorners")
+            # If found, add object points, image points (after refining them)
+            if ret == True:
+                print("        Corners Found")
+                objpoints.append(objp)
+                corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+                imgpoints.append(corners2)
 
-
-    allMTX.append(mtx)
-    allDist.append(dist)
-
-
-MTXStack = np.stack(allMTX,axis=1)
-meanMTX = np.mean(MTXStack,axis=1)
-stdMTX = np.std(MTXStack,axis=1)
-print(meanMTX)
-print(stdMTX)
-
-meanFx = meanMTX[0, 0]
-meanFy = meanMTX[1, 1]
-meanX0 = meanMTX[0, 2]
-meanY0 = meanMTX[1, 2]
-
-#print('meanFx ', meanFx)
-#print('meanFy ', meanFy)
-#print('meanX0 ', meanX0)
-#print('meanY0 ', meanY0)
-
-DISTStack = np.stack(allDist,axis=1)
-meanDIST = np.mean(DISTStack,axis=1)
-stdDist = np.std(DISTStack,axis=1)
-print(meanDIST)
-print(stdDist)
-
-meanK1 = meanDIST[0,0]
-meanK2 = meanDIST[0,1]
-meanP1 = meanDIST[0,2]
-meanP2 = meanDIST[0,3]
-meanK3 = meanDIST[0,4]
-#print('meanK1 ', meanDIST[0,0])
-#print('meanK2 ', meanDIST[0,1])
-#print('meanP1 ', meanDIST[0,2])
-#print('meanP1 ', meanDIST[0,3])
-#print('meanK3 ', meanDIST[0,4])
-
-#Konfidenzintervall 95% bei 5 Samples T- Verteilung = 1,242
-
-uncertantyMTX = 1.242*stdMTX
-uncertantyDIST = 1.242*stdDist
+                # Draw and display the corners
+                img = cv2.drawChessboardCorners(img, (columns,rows), corners2,ret)
+                if saveImages:
+                    utils.saveImagesToDirectory(counter2,img,directory2)
+                counter2 += 1
+                cv2.imshow('img',img)
+                cv2.waitKey(200)
+            else:
+                print("         No Corners Found")
+        print('Found Corners in ' +str(counter2) + ' of ' + str(len(images))+ ' images')
+        print('Detect at least 10 for optimal results')
+        cv2.destroyWindow("img")
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+        print('Matrix:')
+        print(mtx)
+        print('Dist:')
+        print(dist)
+        mean_error = 0
+        meanErrorZeroDist = 0
+        distZero = np.array([0,0,0,0,0],dtype=float)
+        for i in range(len(objpoints)):
+            imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, distZero)
+            error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+            meanErrorZeroDist += error
+            if i == 1:
+                pass
+                # print('Soll')
+                # print(imgpoints[i])
+                # print('Nach Reproduktion')
+                # print(imgpoints2)
+        print("Mean error between Ideal Chessboard Corners and Image Corners: {}".format(meanErrorZeroDist / len(objpoints)))
+        for i in range(len(objpoints)):
+            imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+            error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+            mean_error += error
+            if i == 1:
+                pass
+                # print('Soll')
+                # print(imgpoints[i])
+                # print('Nach Reproduktion')
+                # print(imgpoints2)
+        print("Mean error between projected Objecktpoints using distortion parameters to Points in real Image: {}".format(mean_error / len(objpoints)))
 
 
-print('Parameter inklusive Konfidenzintervalle (95%):')
-print('fx: ', str(meanFx), ' +/- ', uncertantyMTX[0,0] )
-print('fy: ', str(meanFy), ' +/- ', uncertantyMTX[1,1] )
-print('x0: ', str(meanX0), ' +/- ', uncertantyMTX[0,2] )
-print('y0: ', str(meanY0), ' +/- ', uncertantyMTX[1,2] )
-print('K1: ', str(meanK1), ' +/- ', uncertantyDIST[0,0] )
-print('K2: ', str(meanK2), ' +/- ', uncertantyDIST[0,1] )
-print('P1: ', str(meanP1), ' +/- ', uncertantyDIST[0,2] )
-print('P2: ', str(meanP2), ' +/- ', uncertantyDIST[0,3] )
-print('K3: ', str(meanK3), ' +/- ', uncertantyDIST[0,4] )
-#Wait
-print("Take picture to undistort")
-while True:
-    succsess, img = cap.read()
-    cv2.imshow("Calib_Chess", img)
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
-    if cv2.waitKey(1) & 0xff == ord('x'):
-        succsess,image = cap.read()
-        cv2.imshow("Distorted",image)
-        if saveImages:
-            utils.saveImagesToDirectory("_distorted",image, "C:\\Users\\Lars\\Desktop\\TestBilder")
-        undist = utils.undistortFunction(image,meanMTX,meanDIST)
-        cv2.imshow("Undistorted",undist)
-        if saveImages:
-            utils.saveImagesToDirectory("_undistorted",undist, "C:\\Users\\Lars\\Desktop\\TestBilder")
-        cv2.waitKey(2000)
-    cv2.waitKey(1)
+        allMTX.append(mtx)
+        allDist.append(dist)
 
-cv2.destroyAllWindows()
-print('LiveView')
 
-while True:
-    succsess, img = cap.read()
-    cv2.imshow("DistortedLive", img)
-    undist = utils.undistortFunction(img, mtx, dist)
-    cv2.imshow("UndistortedLive", undist)
-    cv2.waitKey(1)
-cv2.destroyAllWindows()
+    MTXStack = np.stack(allMTX,axis=1)
+    meanMTX = np.mean(MTXStack,axis=1)
+    stdMTX = np.std(MTXStack,axis=1)
+    print(meanMTX)
+    print(stdMTX)
+
+    meanFx = meanMTX[0, 0]
+    meanFy = meanMTX[1, 1]
+    meanX0 = meanMTX[0, 2]
+    meanY0 = meanMTX[1, 2]
+
+    #print('meanFx ', meanFx)
+    #print('meanFy ', meanFy)
+    #print('meanX0 ', meanX0)
+    #print('meanY0 ', meanY0)
+
+    DISTStack = np.stack(allDist,axis=1)
+    meanDIST = np.mean(DISTStack,axis=1)
+    stdDist = np.std(DISTStack,axis=1)
+    print(meanDIST)
+    print(stdDist)
+
+    meanK1 = meanDIST[0,0]
+    meanK2 = meanDIST[0,1]
+    meanP1 = meanDIST[0,2]
+    meanP2 = meanDIST[0,3]
+    meanK3 = meanDIST[0,4]
+    #print('meanK1 ', meanDIST[0,0])
+    #print('meanK2 ', meanDIST[0,1])
+    #print('meanP1 ', meanDIST[0,2])
+    #print('meanP1 ', meanDIST[0,3])
+    #print('meanK3 ', meanDIST[0,4])
+
+    #Konfidenzintervall 95% bei 5 Samples T- Verteilung = 1,242
+
+    uncertantyMTX = 1.242*stdMTX
+    uncertantyDIST = 1.242*stdDist
+
+
+    print('Parameter inklusive Konfidenzintervalle (95%):')
+    print('fx: ', str(meanFx), ' +/- ', uncertantyMTX[0,0] )
+    print('fy: ', str(meanFy), ' +/- ', uncertantyMTX[1,1] )
+    print('x0: ', str(meanX0), ' +/- ', uncertantyMTX[0,2] )
+    print('y0: ', str(meanY0), ' +/- ', uncertantyMTX[1,2] )
+    print('K1: ', str(meanK1), ' +/- ', uncertantyDIST[0,0] )
+    print('K2: ', str(meanK2), ' +/- ', uncertantyDIST[0,1] )
+    print('P1: ', str(meanP1), ' +/- ', uncertantyDIST[0,2] )
+    print('P2: ', str(meanP2), ' +/- ', uncertantyDIST[0,3] )
+    print('K3: ', str(meanK3), ' +/- ', uncertantyDIST[0,4] )
+    #Wait
+    return meanMTX,meanDIST,uncertantyMTX,uncertantyDIST
--- main.py
+++ main.py
@@ -1,50 +1,118 @@
+import numpy as np
 import cv2
+import cv2.aruco as aruco
+import os
+import utils
+import math
+import ContourUtils
+import CalibrationWithUncertanty
+from scipy.spatial import distance as dist
 
-
-class Point:
-    x = 0
-    y = 0
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+import pickle
+import glob
 
 
 cap = cv2.VideoCapture(0)
 
-cap.set(2,1280)         #Width
-cap.set(3,720)         #Hight
-cap.set(10,10)        #Brightness
+cap.set(2,1920)
+cap.set(3,1080)
 
-calibrationSquareDim = 0.019    #meters
-chessBoardDimentions = (6,9)
+rows = 6            #17
+columns = 9         #28
+saveImages = False
 
-def cerateKnownBoardPositions(boardSize,squareEdgeLenght,Points):        #Array, float, ArrayofPoints
-    counter = 0
-    for i in range(boardSize[0]):       #hight
-        for j in range(boardSize[1]):   #width
-            Points[counter] = Point(j*squareEdgeLenght,i*squareEdgeLenght)
-    return  Points
+# termination criteria for Subpixel Optimization
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-def getChessBoardcorners(images,foundCorners,showresults):                           #all images + bool visualize or not
-    for img in images:
-        pointbuffer = []
-        success = cv2.findChessboardCorners(img,(9,6),pointbuffer,cv2.CALIB_CB_ADAPTIVE_THRESH)
-        if success:
-            foundCorners.append(pointbuffer)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+squareSize = 30#mm      Beinflusst aber in keiner weise die Matrix
+objp = np.zeros((rows*columns,3), np.float32)
+objp[:,:2] = np.mgrid[0:columns,0:rows].T.reshape(-1,2)*squareSize
 
-        if showresults:
-            cv2.drawChessboardCorners(img,(9,6),foundCorners,success)
-            cv2.imshow("Corners",img)
+runs = 1
 
+meanMTX,meanDIST,uncertantyMTX,uncertantyDIST = CalibrationWithUncertanty.calibrateCamera(cap=cap,rows=rows,columns=columns,squareSize=squareSize,objp=objp,runs=runs,
+                                                                                          saveImages=False)
+print("Take picture to undistort")
+while True:
+    succsess, img = cap.read()
+    cv2.imshow("Calib_Chess", img)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+    if cv2.waitKey(1) & 0xff == ord('x'):
+        succsess,image = cap.read()
+        cv2.imshow("Distorted",image)
+        if saveImages:
+            utils.saveImagesToDirectory("_distorted",image, "C:\\Users\\Lars\\Desktop\\TestBilder")
+        undist = utils.undistortFunction(image,meanMTX,meanDIST)
+        cv2.imshow("Undistorted",undist)
+        if saveImages:
+            utils.saveImagesToDirectory("_undistorted",undist, "C:\\Users\\Lars\\Desktop\\TestBilder")
+        cv2.waitKey(2000)
+    cv2.waitKey(1)
 
+cv2.destroyAllWindows()
+print('LiveView')
 
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+
+distZero = np.array([0, 0, 0, 0, 0], dtype=float)
+
+showConts = True
 
 while True:
-    success, img = cap.read()
-    if success:
-        cv2.imshow("Video", img)                               #Video Import from Webcam
-        if cv2.waitKey(1) & 0xFF ==ord('q'):
-            break
+    timer = cv2.getTickCount()
+    succsess, img = cap.read()
+    # cv2.imshow("DistortedLive", img)
+    undist = utils.undistortFunction(img, meanMTX, meanDIST)
+    cv2.waitKey(1)
+    if showConts:
+        imgContours, conts = ContourUtils.getContours(undist, cThr=(220, 250), draw=True)
+        if len(conts) != 0:
+            for obj in conts:
+                cv2.polylines(imgContours, [obj[2]], True, (0, 255, 0), 2)
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(undist, aruco_dict)
+    print("Corners",corners)
+    corners = np.array(corners)
+    #print("Shape", corners.shape)
+    #print("CornersNEWSHAPE", corners)
+    reorderd = ContourUtils.reorder(corners)
+    print("reorderd",reorderd)
+    if reorderd is not None:
+        print(reorderd[3])
+        print("SHAPE",reorderd[3].shape)
+        (tltrX, tltrY) = ContourUtils.midpoint(reorderd[0], reorderd[1]) #top left,top right
+        (blbrX, blbrY) = ContourUtils.midpoint(reorderd[2], reorderd[3]) #bottom left, botto right
+        (tlblX, tlblY) = ContourUtils.midpoint(reorderd[0], reorderd[2])
+        (trbrX, trbrY) = ContourUtils.midpoint(reorderd[1], reorderd[3])
+        cv2.circle(undist, (int(tltrX), int(tltrY)), 1, (255, 255, 0), 2)
+        cv2.circle(undist, (int(blbrX), int(blbrY)), 1, (255, 255, 0), 2)
+        cv2.circle(undist, (int(tlblX), int(tlblY)), 1, (255, 255, 0), 2)
+        cv2.circle(undist, (int(trbrX), int(trbrY)), 1, (255, 255, 0), 2)
+        cv2.line(undist, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+                 (255, 0, 255), 2)
+        cv2.line(undist, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+                 (255, 0, 255), 2)
 
-cv2.waitKey()
+        dY = int(dist.euclidean((tltrX, tltrY), (blbrX, blbrY)))
+        dX = int(dist.euclidean((tlblX, tlblY), (trbrX, trbrY)))
+        cv2.putText(undist, str(dY),(5, 50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255))
+        cv2.putText(undist, str(dX), (5, 75), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255))
+
+    aruco.drawDetectedMarkers(undist, corners)
+    rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, 0.053, meanMTX, meanDIST)  # größße des marker in m
+    # rvecZeroDist, tvecZeroDist, _ = aruco.estimatePoseSingleMarkers(corners, 0.053, mtx, distZero)  # größße des marker in m
+    if rvec is not None and tvec is not None:
+        aruco.drawAxis(undist, meanMTX, meanDIST, rvec, tvec, 0.05)
+        cv2.putText(undist, "%.1f cm -- %.0f deg" % ((tvec[0][0][2] * 100), (rvec[0][0][2] / math.pi * 180)), (0, 230),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (244, 244, 244))
+        #print(rvec)
+        #print(tvec)
+    else:
+        print("No Marker found")
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+    cv2.putText(undist, str(int(fps)), (5, 25), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0))
+    cv2.imshow("UndistortedLive", undist)
+    cv2.imshow("DistortedLive", img)
+
+cv2.destroyAllWindows()
