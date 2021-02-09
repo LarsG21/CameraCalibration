1adf4bd9c3bf5fe0996ea19c35529b2e6e2da151
Lars UniPC <larsgudjons@gmail.com>
2021-02-09 18:50:35 +0100

    Epsilon wert besser aufgelöst Slider geht nun viel besser (Gleiches mit Canny usw versuchen)

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ def updateTrackBar():
     cannyHigh = cv2.getTrackbarPos("Canny Threshold High", "Edge Detection Settings")
     noGauss = cv2.getTrackbarPos("Number of Gauss Filters", "Edge Detection Settings")
     minArea = cv2.getTrackbarPos("Minimum Area of Contours", "Edge Detection Settings")
-    epsilon = (cv2.getTrackbarPos("Epsilon (Resolution of Poly Approximation)", "Edge Detection Settings")) / 100
+    epsilon = (cv2.getTrackbarPos("Epsilon (Resolution of Poly Approximation)", "Edge Detection Settings")) / 1000
     showFilters = bool(cv2.getTrackbarPos("Show Filters", "Edge Detection Settings"))
 
     return cannyLow, cannyHigh, noGauss, minArea, epsilon, showFilters
